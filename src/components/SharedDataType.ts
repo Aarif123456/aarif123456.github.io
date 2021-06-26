110ff880b00af8e1bbc93873f53a973846757ed5
aarif123456 <abdullahmeo11@gmail.com>
2021-06-25 21:06:40 -0400

    Add shared data

---
+++ src/components/SharedDataType.ts
@@ -0,0 +1,23 @@
+export interface SharedData {
+    basic_info?: SharedBasicInfo;
+    skills?: SharedSkills;
+}
+export interface SharedBasicInfo {
+    name: string;
+    titles?: [string];
+    social?: [SharedSocialEntity];
+    image: string;
+}
+export interface SharedSocialEntity {
+    name: string;
+    url: string;
+    class: string;
+}
+export interface SharedSkills {
+    icons?: [IconsEntity];
+}
+export interface IconsEntity {
+    name: string;
+    class: string;
+    level: string;
+}
