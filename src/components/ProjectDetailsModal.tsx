fe336a9aac489f47f10312463f962ec72e1090e9
aarif123456 <abdullahmeo11@gmail.com>
2021-06-25 21:05:48 -0400

    Add Project section

---
+++ src/components/ProjectDetailsModal.tsx
@@ -0,0 +1,85 @@
+import { FunctionComponent } from 'react';
+import { Modal } from 'react-bootstrap';
+import AwesomeSlider from 'react-awesome-slider';
+import AwesomeSliderStyles from '../scss/light-slider.scss';
+import AwesomeSliderStyles2 from '../scss/dark-slider.scss';
+import 'react-awesome-slider/dist/custom-animations/scale-out-animation.css';
+import { ProjectsEntity } from '.';
+
+type Props = {
+    data?: ProjectsEntity;
+    onHide: () => void;
+    /* Figure out how to extend from modal props https://react-bootstrap.github.io/components/modal/#modals-props*/
+    show?: boolean;
+    animation?: boolean;
+    autoFocus?: boolean;
+};
+
+export const ProjectDetailsModal: FunctionComponent<Props> = (props) => {
+    const { data, onHide } = props;
+    if (data === undefined) return <div />;
+    const technologies = data.technologies ?? [];
+    const images = data.images ?? [];
+    const title = data.title;
+    const description = data.description;
+    const url = data.url;
+    const tech = technologies?.map((icons, i) => {
+        return (
+            <li className='list-inline-item mx-3' key={i}>
+                <span>
+                    <div className='text-center'>
+                        <i className={icons.class} style={{ fontSize: '300%' }}>
+                            <p className='text-center' style={{ fontSize: '30%' }}>
+                                {icons.name}
+                            </p>
+                        </i>
+                    </div>
+                </span>
+            </li>
+        );
+    });
+    const img = images.map((elem, i) => {
+        return <div key={i} data-src={elem} />;
+    });
+
+    return (
+        <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered className='modal-inside'>
+            <span onClick={onHide} className='modal-close'>
+                <i className='fas fa-times fa-3x close-icon'></i>
+            </span>
+            <div className='col-md-12'>
+                <div className='col-md-10 mx-auto' style={{ paddingBottom: '50px' }}>
+                    <div className='slider-tab'>
+                        <span
+                            className='iconify slider-iconfiy'
+                            data-icon='emojione:red-circle'
+                            data-inline='false'
+                            style={{ marginLeft: '5px' }}></span>{' '}
+                        &nbsp; <span className='iconify slider-iconfiy' data-icon='twemoji:yellow-circle' data-inline='false'></span> &nbsp;{' '}
+                        <span className='iconify slider-iconfiy' data-icon='twemoji:green-circle' data-inline='false'></span>
+                    </div>
+                    <AwesomeSlider
+                        cssModule={[AwesomeSliderStyles, AwesomeSliderStyles2]}
+                        animation='scaleOutAnimation'
+                        className='slider-image'>
+                        {img}
+                    </AwesomeSlider>
+                </div>
+                <div className='col-md-10 mx-auto'>
+                    <h3 style={{ padding: '5px 5px 0 5px' }}>
+                        {title}
+                        {url ? (
+                            <a href={url} target='_blank' rel='noopener noreferrer' className='link-href'>
+                                <i className='fas fa-external-link-alt' style={{ marginLeft: '10px' }}></i>
+                            </a>
+                        ) : null}
+                    </h3>
+                    <p className='modal-description'>{description}</p>
+                    <div className='col-md-12 text-center'>
+                        <ul className='list-inline mx-auto'>{tech}</ul>
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    );
+};
---
+++ src/components/Projects.tsx
@@ -0,0 +1,66 @@
+import { FunctionComponent, useReducer, Reducer } from 'react';
+import { ProjectDetailsModal } from './ProjectDetailsModal';
+import { ProjectsEntity, BasicInfo } from '.';
+
+type Props = {
+    resumeBasicInfo?: BasicInfo;
+    resumeProjects?: [ProjectsEntity];
+};
+
+type ModalData = {
+    detailsModalShow: boolean;
+    deps?: ProjectsEntity;
+};
+export const Projects: FunctionComponent<Props> = (props) => {
+    const [state, setState] = useReducer<Reducer<ModalData, Partial<ModalData>>>((state, newState) => ({ ...state, ...newState }), {
+        detailsModalShow: false,
+        deps: undefined
+    });
+
+    const { resumeProjects, resumeBasicInfo } = props;
+    const detailsModalShow = (data: ProjectsEntity) => {
+        setState({ detailsModalShow: true, deps: data });
+    };
+
+    const detailsModalClose = () => setState({ detailsModalShow: false });
+    if (!(resumeProjects && resumeBasicInfo)) return <div />;
+    const sectionName = resumeBasicInfo.section_name.projects;
+    const projects = resumeProjects.map(function (projects) {
+        const projectImage = 'images' in projects ? (projects.images as [string]) : [];
+        return (
+            <div className='col-sm-12 col-md-6 col-lg-4' key={projects.title} style={{ cursor: 'pointer' }}>
+                <span className='portfolio-item d-block'>
+                    <div className='foto' onClick={() => detailsModalShow(projects)}>
+                        <div>
+                            {projectImage.length > 0 && (
+                                <img
+                                    src={projectImage[0] ?? ''}
+                                    alt='projectImages'
+                                    height='230'
+                                    style={{ marginBottom: 0, paddingBottom: 0, position: 'relative' }}
+                                />
+                            )}
+                            <span className='project-date'>{projects.startDate}</span>
+                            <br />
+                            <p className='project-title-settings mt-3'>{projects.title}</p>
+                        </div>
+                    </div>
+                </span>
+            </div>
+        );
+    });
+
+    return (
+        <section id='portfolio'>
+            <div className='col-md-12'>
+                <h1 className='section-title' style={{ color: 'black' }}>
+                    <span>{sectionName}</span>
+                </h1>
+                <div className='col-md-12 mx-auto'>
+                    <div className='row mx-auto'>{projects}</div>
+                </div>
+                <ProjectDetailsModal show={state.detailsModalShow} onHide={detailsModalClose} data={state.deps} />
+            </div>
+        </section>
+    );
+};
