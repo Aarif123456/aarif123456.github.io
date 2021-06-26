f9339795c3e10b8fe38a0b93af66b7ccf8123198
aarif123456 <abdullahmeo11@gmail.com>
2021-06-25 21:06:08 -0400

    Add Skill section

---
+++ src/components/Skills.tsx
@@ -0,0 +1,43 @@
+import { FunctionComponent } from 'react';
+import { BasicInfo, SharedSkills } from '.';
+
+type Props = {
+    resumeBasicInfo?: BasicInfo;
+    sharedSkills?: SharedSkills;
+};
+
+export const Skills: FunctionComponent<Props> = (props) => {
+    const { sharedSkills, resumeBasicInfo } = props;
+    if (!(sharedSkills && resumeBasicInfo)) return <div />;
+    const sectionName = resumeBasicInfo.section_name.skills;
+    const skills = sharedSkills.icons?.map(function (skills, i) {
+        return (
+            <li className='list-inline-item mx-3' key={i}>
+                <span>
+                    <div className='text-center skills-tile'>
+                        <i className={skills.class} style={{ fontSize: '220%' }}>
+                            <p className='text-center' style={{ fontSize: '30%', marginTop: '4px' }}>
+                                {skills.name}
+                            </p>
+                        </i>
+                    </div>
+                </span>
+            </li>
+        );
+    });
+
+    return (
+        <section id='skills'>
+            <div className='col-md-12'>
+                <div className='col-md-12'>
+                    <h1 className='section-title'>
+                        <span className='text-white'>{sectionName}</span>
+                    </h1>
+                </div>
+                <div className='col-md-12 text-center'>
+                    <ul className='list-inline mx-auto skill-icon'>{skills}</ul>
+                </div>
+            </div>
+        </section>
+    );
+};
